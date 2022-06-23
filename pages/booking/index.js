8422c339612d13d964ffb5aed0134002ff49b224
Dhruv Khanna <42564960+dhruvkhanna98@users.noreply.github.com>
2022-06-23 11:33:32 -0700

    Move calendar functionality to index

--- pages/booking/index.js
+++ pages/booking/index.js
@@ -9,6 +9,10 @@ import { css } from "@emotion/react"
 import LocationCard from "../../components/LocationCard";
 import GameCard from "../../components/GameCard";
 import Calendar from "../../components/Calendar";
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+
 
 
 export default function NewBookingPage() {
@@ -34,7 +38,8 @@ export default function NewBookingPage() {
     
     const [groupSize, setGroupSize] = useState(3);
     // const [date, setDate] = useState(moment().format('YYYY-MM-DD'));
-    const [date, setDate] = useState("");
+    const [dateStr, setDateStr] = useState('');
+    const [date, setDate] = useState(new Date());
     const [duration, setDuration] = useState(60);
     const [availabilities, setAvailabilities] = useState([]);
     const [location, setLocation] = useState('');
@@ -132,8 +137,32 @@ export default function NewBookingPage() {
     else {
         return (
          <Box sx={styles.root}> 
-                <Calendar dateFunc={setDate} />
-                <h1> {date}</h1>
+             <Grid container
+                spacing={2}
+                direction='column'
+                justifyContent="center" 
+                alignItems="center">
+                <Grid item xs='auto'>
+                    <h3 style={{fontSize: "30px", margin: "0"}}>Choose a session</h3>
+                </Grid>
+                <Grid item xs='auto'>
+                <h5 style={{fontSize: "20px", margin: "0"}}>Select a time</h5>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <StaticDatePicker
+                        displayStaticWrapperAs="desktop"
+                        openTo="day"
+                        date={date}
+                        onChange={(newValue) => {
+                        setDate(newValue);
+                        setDateStr(moment(date).format('YYYY-MM-DD'));
+                        }}/>
+                </LocalizationProvider>
+                </Grid>
+                <Grid item xs='auto'>
+                    <h4> Selected date is {dateStr}</h4>
+                </Grid>
+                
+            </Grid>
        </Box>)
     }
 }
