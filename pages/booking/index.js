23d1478731ab92054a6e0965851b01bea49556d7
nancy_wan <nancy_wan@thecdm.ca>
2022-07-02 21:13:02 -0700

    Removed redundant styles in booking index page

--- pages/booking/index.js
+++ pages/booking/index.js
@@ -1,48 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Box } from '@mui/system';
-import { IconButton, Button, Grid } from "@mui/material";
 import { useRouter } from 'next/router';
 import moment from 'moment';
-import { css } from "@emotion/react"
 
 import LocationPage from "../../components/booking/location/LocationPage";
 import GamePage from "../../components/booking/game/GamePage";
 import CalendarPage from "../../components/booking/calendar/CalendarPage";
 
 export default function NewBookingPage() {
-
-    const styles = {
-        root: css`
-            background-color: #FAF9F6;
-          `,
-        grid: css`
-            margin: 2rem auto 0 auto;
-          `,
-        cardButton: css`
-            width: 30rem;
-            height: 10rem;
-        `,
-        gameButton: css`
-            margin: 6rem 2rem 0 auto;
-            display: block;
-            textAlign: center;
-            color: white;
-            width: 20rem;
-            height: 5rem;
-        `,
-        timeButton: css`
-            background-color: #FFFFFF;
-            color: black;
-        `,
-
-        timeSelectionButton: css`
-            margin: 2rem auto;
-            width: 20rem;
-            height: 3rem;
-            
-        `
-    }
-
     const router = useRouter()
 
     const groupSize = 3;
